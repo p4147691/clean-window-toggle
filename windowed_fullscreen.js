0317c0b57fba1f4d80411906dc89f0abcb026cf7
p4147691 <255136024+p4147691@users.noreply.github.com>
2026-09-04 00:58:26 +0900

    Fix blank new-tab return and restore focused command routing

--- windowed_fullscreen.js
+++ windowed_fullscreen.js
@@ -1,5 +1,5 @@
 (() => {
-const RUNTIME_VERSION = "2.3.21";
+const RUNTIME_VERSION = "2.3.23";
 const RUNTIME_KEY = "__cleanWindowRuntimeV1";
 const MESSAGE_BRIDGE_KEY = "__cleanWindowRuntimeMessageBridgeV1";
 const previousRuntime = globalThis[RUNTIME_KEY];
